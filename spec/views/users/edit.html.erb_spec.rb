d68841db72539f1f48b3226fd7227594ad814d67
Nate Norberg <nate.norberg@oracle.com>
2014-07-15 15:05:14 -0600

    Mocked user to fix tests

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "users/edit", :type => :view do
   before(:each) do
+    mock_user = stub_model(User)
+    mock_user.stub(:is_admin?).and_return(true)
+    view.stub(:current_user).and_return(mock_user)
     @user = assign(:user, User.create!(
       :name => "MyString",
       :email => "MyString",
