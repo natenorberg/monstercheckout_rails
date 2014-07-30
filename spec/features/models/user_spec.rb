f16a5ebd60eb7d3df708653ef25ec9c453f4a282
Nate Norberg <nate.norberg@oracle.com>
2014-07-30 15:49:38 -0600

    Made tests use :expect syntax instead of :should for non=integration tests

--- spec/features/models/user_spec.rb
+++ spec/features/models/user_spec.rb
@@ -14,62 +14,57 @@ require 'rails_helper'
 
 describe "User" do
   
-  before { @user = User.new(name: "Example User", 
-                            email: "user@example.com", 
-                            password: "password1", 
-                            password_confirmation: "password1") }
+  before { @user = FactoryGirl.create(:user) }
 
   subject { @user }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:password_digest) }
+  it "should respond to attributes" do
+    expect(@user).to respond_to(:name)
+    expect(@user).to respond_to(:email)
+    expect(@user).to respond_to(:password)
+    expect(@user).to respond_to(:password_confirmation)
+    expect(@user).to respond_to(:password_digest)
+    expect(@user).to respond_to(:is_admin?)
+  end
 
-  it { should be_valid }
+  it "should have a valid factory" do
+    expect(@user).to be_valid
+  end
 
   # Name tests
-  describe "when name is blank" do
-    before {@user.name = ""}
-    it { should_not be_valid }
+  it "is invalid without a name" do
+    @user.name = ''
+    expect(@user).to_not be_valid
   end
 
   # Email tests
-  describe "when email is blank" do
-    before {@user.email = ""}
-    it { should_not be_valid }
+  it "is invalid without an email" do
+    @user.email = ''
+    expect(@user).to_not be_valid
   end
 
-  describe "when email format is invalid" do 
-    it "should be invalid" do 
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+  it "is invalid when email format is invalid" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                        foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
-      end 
-    end 
-  end 
-
-  describe "when email format is valid" do 
-    it "should be valid" do 
-        addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-        addresses.each do |valid_address|
-          @user.email = valid_address
-          @user.should be_valid
-        end 
-    end 
-  end 
-
-  describe "when email has already been taken" do
-    before do
-      other_user = @user.dup
-      other_user.email = @user.email.upcase
-      other_user.save
+    addresses.each do |invalid_address|
+      @user.email = invalid_address
+      expect(@user).to_not be_valid
     end
+  end
 
-    it { should_not be_valid }
+  it "is valid when email format is valid" do
+    addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+    addresses.each do |valid_address|
+      @user.email = valid_address
+      expect(@user).to be_valid
+    end
+  end
+
+  it "is invalid when email has already been taken" do
+    other_user = @user.dup
+    other_user.email = @user.email.upcase
+
+    expect(other_user).to_not be_valid
   end
 
   describe "email address with mixed case" do 
@@ -78,24 +73,25 @@ describe "User" do
     it "should be saved as all lower-case" do
         @user.email = mixed_case_email
         @user.save
-        @user.reload.email.should == mixed_case_email.downcase
+
+        expect(@user.reload.email).to eq(mixed_case_email.downcase)
     end 
   end 
 
   # Password/confirmation tests
-  describe "when password is blank" do
-    before { @user.password = @user.password_confirmation = "" }
-    it { should_not be_valid }
+  it "should be invalid when password is blank" do
+    @user.password = @user.password_confirmation = ''
+    expect(@user).to_not be_valid
   end
 
-  describe "when password does not match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
+  it "should be invalid when password doesn't match confirmation" do
+    @user.password_confirmation = 'mismatch'
+    expect(@user).to_not be_valid
   end
 
-  describe "when password is less than 6 characters" do
-    before { @user.password = @user.password_confirmation = 'n' * 5 }
-    it { should_not be_valid }
+  it "should be invalid when password is too short" do
+    @user.password = @user.password_confirmation = 'n' * 5
+    expect(@user).to_not be_valid
   end
 
   # Authentication tests
@@ -103,21 +99,21 @@ describe "User" do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
 
-    describe "with valid password" do
-      it { should == found_user.authenticate(@user.password) }
+    it "should authenticate with valid password" do
+      expect(@user).to eq(found_user.authenticate(@user.password))
     end
 
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    it "should not authentice with invalid password" do
+      user_for_invalid_password = found_user.authenticate('invalid')
 
-      it { should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should eq(false) }
+      expect(@user).to_not eq(user_for_invalid_password)
+      expect(user_for_invalid_password).to eq(false)
     end
   end
 
   # Remember token tests
-  describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
+  it "should create remember_token on save" do
+    @user.save
+    expect(@user.remember_token).to_not be_blank
   end
 end
