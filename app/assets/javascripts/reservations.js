e7c3f5312ea0081121d51d1f37a060ff87ce6af5
Nate Norberg <nate.norberg@oracle.com>
2014-08-27 11:37:32 -0600

    Hid checkout comments until equipment is checked off

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -5,8 +5,14 @@ angular.module("admin-reservation-index", [])
 });
 
 $(".check").click(function() {
-    if (!$('input.check[type=checkbox]:not(:checked)').length)
+    if (!$('input.check[type=checkbox]:not(:checked)').length) {
         document.getElementById("checkout_button").disabled=false;
+	    $("#check_out_comments_field").show();
+	}
     else
         document.getElementById("checkout_button").disabled=true;
+});
+
+$( document ).ready(function() {
+  $("#check_out_comments_field").hide();
 });
