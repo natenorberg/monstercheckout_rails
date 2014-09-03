9af066c5153625092db0aa1342a0a0a77380fb2b
Nate Norberg <nate.norberg@oracle.com>
2014-09-03 17:23:48 -0600

    Got scheduling working on create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :edit, :update, :destroy, :approve, :deny, :checkout, :checkout_update, :checkin, :checkin_update]
-  before_action :set_equipment, only: [:new, :edit]
+  before_action :set_equipment, only: [:new, :create, :edit]
   before_filter :user_signed_in
   before_filter :current_user_or_admin, only: [:destroy]
   before_filter :user_is_admin, only: [:approve, :deny]
@@ -38,10 +38,15 @@ class ReservationsController < ApplicationController
   def create
     format_time_input
     params[:reservation][:equipment_ids] ||= []
+    @conflicts = conflicts
     @reservation = Reservation.new(reservation_params)
     @reservation.status = :requested
     respond_to do |format|
-      if @reservation.save
+      if @conflicts.any?
+        @reservation.errors.add(:base, 'There are conflicts')
+        format.html { render :new }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      elsif @reservation.save
         update_quantities
         format.html { redirect_to @reservation, flash: { success: 'Reservation was successfully created.' } }
         format.json { render :show, status: :created, location: @reservation }
@@ -200,6 +205,75 @@ class ReservationsController < ApplicationController
 
     def reservation_can_be_checked_in
       redirect_to root_path unless @reservation.can_checkin?
+    end
+
+    def conflicts
+      # Get the other reservations to check agains
+      # TODO: Be more selective here
+      if params[:id]
+        other_reservations = Reservation.where.not(id: params[:id])
+      else
+        other_reservations = Reservation.all
+      end
+
+      overlapping_reservations = []
+      other_reservations.each do |reservation|
+        if overlap(reservation, params[:reservation][:out_time], params[:reservation][:in_time])
+          overlapping_reservations << reservation        
+        end
+      end
+
+      conflicting_equipment(params[:reservation][:equipment_ids], params[:reservation][:quantity], overlapping_reservations)
+    end
+
+    # TODO: This logic probably belongs in its own class
+    def overlap(reservation, start_time, end_time)
+      if start_time < reservation.out_time
+        # New reservation starts before other one
+        if end_time < reservation.in_time
+          return false # Other reservation returned before new one checked out
+        else
+          return true
+        end
+
+      else
+        if start_time > reservation.in_time
+          return false # Reservation is returned before requested out_time
+        else
+          return true
+        end
+      end
+    end
+
+    def conflicting_equipment(equipment_ids, quantities, other_reservations)
+      equipment = Equipment.where(:id => equipment_ids)
+      conflicts = {}
+      total_quantities = {}
+
+      equipment.each do |item|
+        # Start with the total number of each item and substract the quantities from the reservations
+        # that overlap. If this goes below 0, then there's a conflict
+        total_quantities[item.id] = item.quantity - quantities[item.id.to_s].to_i
+      end
+
+      # Triple nested loops are bad, but they can't be very deep in this scenario and I'm not sure there's another way
+      other_reservations.each do |reservation|
+        reservation.reservation_equipment.each do |other_item|
+          equipment.each do |item|
+
+            if item == other_item.equipment
+              total_quantities[item.id] -= other_item.quantity
+
+              if total_quantities[item.id] < 0
+                conflicts[item.id] = reservation
+              end
+            end
+
+          end
+        end
+      end
+
+      conflicts
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
