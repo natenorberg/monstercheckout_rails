bea832c24201c957dbd47c6e286bac0f24311ec0
Nate Norberg <nate.norberg@oracle.com>
2014-09-24 18:08:27 -0600

    Email templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@montana.edu"
+  layout 'email'
 
   def need_approval_email(user, reservation)
     @reservation = reservation
+    @show_button_bar = true
 
     mail(to: user.email, subject: 'A reservation needs your approval')
   end
