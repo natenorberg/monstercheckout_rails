40144c1733111d0d1879ae3dc9c12cf3a8418897
Nate Norberg <nate.norberg@oracle.com>
2015-07-08 22:24:08 -0600

    Made equipment category filters visible as non-admin

--- spec/views/equipment/index.html.erb_spec.rb
+++ spec/views/equipment/index.html.erb_spec.rb
@@ -29,4 +29,15 @@ RSpec.describe 'equipment/index', :type => :view do
     assert_select 'tr>td', :text => 'Brand'.to_s, :count => 2
     assert_select 'tr>td', :text => 1.to_s, :count => 2
   end
+
+  describe 'when user is not an admin' do
+    before do
+      @user.stub(:is_admin?).and_return(false)
+    end
+
+    it 'renders category selectors' do
+      render
+      assert_select 'ul.nav.nav-pills'
+    end
+  end
 end
