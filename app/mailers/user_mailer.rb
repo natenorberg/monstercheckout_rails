cf46496a5366f6a62f9d35feebe9a514a39a49c9
Nate Norberg <nate.norberg@oracle.com>
2015-08-24 20:21:43 -0600

    Fixed issue with creating a reservation when email is not set up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: "noreply@montana.edu"
   layout 'email'
 
+  before_filter :check_environment
+
   def welcome_email(user)
     @user = user
     mail(to: user.email, subject: 'Welcome to MONSTER Checkout')
@@ -37,4 +39,12 @@ class UserMailer < ActionMailer::Base
     @show_button_bar = true
     mail(to: reservation.user.email, subject: 'Your reservation has been returned')
   end
+
+  private
+
+    def check_environment
+      if ENV['GMAIL_USERNAME'].nil? || ENV['GMAIL_PASSWORD'].nil?
+        mail.perform_deliveries = false
+      end
+    end
 end
